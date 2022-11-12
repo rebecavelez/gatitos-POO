c68e7b65f4392c374af9fa70d4b7f098a485931d
megumivelez <rebecavelez.r@gmail.com>
2022-11-11 20:19:01 -0600

    se agregan gatitos

--- poo.js
+++ poo.js
@@ -4,18 +4,24 @@ class gatito {
     raza = "";
     caracter = "";
 
-    //4- Agregar constructores 
-    //Necesito construir con los atributos que ya defini en la clase
+
 
     constructor(nombre, edad, raza, caracter) {
-        //funcion especial para construir donde necesito esos parametros 
+
         this.nombre = nombre;
         this.edad = edad;
         this.raza = raza;
         this.caracter = caracter;
     }
 
-    //3- Definir los metodos que seran definidos como funciones o acciones 
+    comer() {
+
+        console.log("ñam ñam");
+    }
+    arañar() {
+
+        console.log("squash squash");
+    }
 
     maullar() {
         console.log("miau");
@@ -30,14 +36,7 @@ class gatito {
         console.log("prrrrrr");
     }
 
-    // arañar();
-    //comer();
-
-
-
 }
-
-//5-para empezar a crear o instanciar objetos (gatos), vamos a utilizar la palabra reservada new nombreClass
 
 var Garfield = new gatito("Garfield", 9, "naranja", "dormilon");
 var Felix = new gatito("Felix", 4, "caricaturas", "amistoso");
@@ -48,35 +47,15 @@ var Chococat = new gatito("Chococat", 7, "caricaturas", "simpatico");
 var Nermal = new gatito("Nermal", 1, "cartoon", "enfadoso")
 
 var Penelope = new gatito("Penelope", 9, "blanca", "linda");
-var Felix = new gatito("Felix", 4, "caricaturas", "amistoso");
-var Kitty = new gatito("Kitty", 1, "kawaii", "tranquilo");
+var Ruby = new gatito("Ruby", 11, "callejera", "la mas bella del mundo");
+var Tori = new gatito("Tori", 1, "callejera", "enojona");
 
-var Garfield = new gatito("Garfield", 9, "naranja", "dormilon");
-var Felix = new gatito("Felix", 4, "caricaturas", "amistoso");
-var Kitty = new gatito("Kitty", 1, "kawaii", "tranquilo");
-
-var Garfield = new gatito("Garfield", 9, "naranja", "dormilon");
-var Felix = new gatito("Felix", 4, "caricaturas", "amistoso");
-var Kitty = new gatito("Kitty", 1, "kawaii", "tranquilo");
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var Tom = new gatito("Tom", 9, "gris", "enojon");
+var Silvestre = new gatito("Silvestre", 4, "caricaturas", "rencoroso");
+var Java = new gatito("Java", 1, "de manchas", "amargada");
+var Pelusita = new gatito("Pelusita", 9, "fina", "observadora");
+var Bambina = new gatito("Bambina", 4, "cola rota", "amistosa");
+var Pocky = new gatito("Pocky", 1, "siames", "miedosa");
 
 
 console.log(Garfield);
@@ -85,6 +64,20 @@ Garfield.dormir();
 Felix.ronronear();
 
 
+console.log(Pocky);
+Garfield.maullar();
+Garfield.dormir();
+Felix.ronronear();
 
+console.log(Nermal);
+Nermal.arañar();
 
+console.log(Pelusita);
+Pelusita.ronronear();
+
+console.log(Penelope);
+Penelope.maullar();
+
+console.log(Tori);
+Tori.dormir();
 
